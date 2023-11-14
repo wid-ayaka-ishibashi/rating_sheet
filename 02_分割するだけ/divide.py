26683bb228f83eb1d0d70741c95e862fe4a4f07b
wid-ayaka-ishibashi <aya_ishibashi@worksid.co.jp>
2023-11-15 02:34:27 +0900

    行間修正

--- 02_分割するだけ/divide.py
+++ 02_分割するだけ/divide.py
@@ -7,22 +7,24 @@ divide_pre = "01_分割前"
 divide_after = "02_分割後"
 save_file_name = "2023年上期評価シート("
 
-for folder, subFolder, files in os.walk(divide_pre):
-    for file in files:
-    #       ext = os.path.splitext(file)[0] #拡張子変更考慮
-    #        files = [i for i in files if "_評価済み" in file]
-        wb = px.load_workbook(folder + "\\" + file)
-        sheetnames = wb.sheetnames
-        sheetnames = [i for i in sheetnames if ("【" and "】") not in i]
-        for sheetname in sheetnames:
-            wb = px.load_workbook(folder + "\\" + file)
-            for ws in wb.worksheets:
-                if ws.title == sheetname:
-                    continue
-                else:
-                    wb.remove(ws)
-                ws = wb[sheetname]
-                name = ws["A2"].value
-            wb.save(divide_after + "\\" + save_file_name + name + ").xlsx")
+# ファイル名のリストを作る
+files = [file for folder, subFolder, files in os.walk(divide_pre) for file in files]
+
+for file in files:
+    wb = px.load_workbook(divide_pre + "\\" + file)
+    sheetnames = [i for i in wb.sheetnames if ("【" and "】") not in i]
+
+    for sheetname in sheetnames:
+        wb = px.load_workbook(divide_pre + "\\" + file)
+
+        # 不要なシートを削除する
+        for ws in wb.worksheets:
+            if ws.title != sheetname:
+                wb.remove(ws)
+        ws = wb[sheetname]
+        # フルネームを取得する
+        name = ws["A2"].value
+
+        wb.save(divide_after + "\\" + save_file_name + name + ").xlsx")
 
 # %%
