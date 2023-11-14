4d34884fee015002575e2bc1e42fad7e0970e1c7
wid-ayaka-ishibashi <aya_ishibashi@worksid.co.jp>
2023-11-14 23:51:48 +0900

    不要シート消して保存のループ

--- 02_分割するだけ/divide.py
+++ 02_分割するだけ/divide.py
@@ -1,23 +1,33 @@
 # %%
 import os
-import openpyxl as opxl
+import openpyxl as px
 from IPython.display import display
 
 divide_pre = "01_分割前"
 divide_after = "02_分割後"
-save_file_name = "2023年上期評価シート("
+save_file_name1 = "2023年上期評価シート("
 
 for folder, subFolder, files in os.walk(divide_pre):
     for file in files:
 #        ext = os.path.splitext(file)[0] #拡張子変更考慮
         if "_評価済み" in file:
-            wb = opxl.load_workbook(folder + "\\" + file)
-            sheetnames = wb.sheetnames
-            for sheetname in sheetnames:
+            wb = px.load_workbook(folder + "\\" + file)
+            i = 0
+            for sheetname in wb.sheetnames:
                 if "【" and "】" not in sheetname:
-                    sheet = wb[sheetname]
-                    name = sheet["A2"].value
-                    wb.remove(sheet)
-                    wb.save(divide_after + "\\" + save_file_name + name + ").xlsx")
+                    i = i+1
+                try:
+                    ws_target = wb.worksheets[i].title
+                except:
+                    break
+                    ws = wb[sheetname]
+                    peple_fullname = ws["A2"].value
+                    for sheetname in wb.sheetnames:
+                        #[ws_target]以外は削除
+                        if sheetname.title == ws_target:
+                            continue
+                        else:
+                            wb.remove(sheetname)
+                        new_wb.save(divide_after + "\\" + save_file_name1 + peple_fullname + ").xlsx")
 
 # %%
