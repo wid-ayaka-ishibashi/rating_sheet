f1671d6543fc3dadda3f5ea44fcd144a4c1b8a40
wid-ayaka-ishibashi <aya_ishibashi@worksid.co.jp>
2023-11-14 19:48:51 +0900

    分割コード作成

--- 02_分割するだけ/divide.py
+++ 02_分割するだけ/divide.py
@@ -3,14 +3,21 @@ import os
 import openpyxl as opxl
 from IPython.display import display
 
-for root, dirs, files in os.walk("./01_分割前"):
-    for file in files:
-        ext = os.path.splitext(file)[1]
-        print(ext)
+divide_pre = "01_分割前"
+divide_after = "02_分割後"
+save_file_name = "2023年上期評価シート("
 
+for folder, subFolder, files in os.walk(divide_pre):
+    for file in files:
+#        ext = os.path.splitext(file)[0] #拡張子変更考慮
+        if "_評価済み" in file:
+            wb = opxl.load_workbook(folder + "\\" + file)
+            sheetnames = wb.sheetnames
+            for sheetname in sheetnames:
+                if "【" and "】" not in sheetname:
+                    sheet = wb[sheetname]
+                    name = sheet["A2"].value
+                    wb.remove(sheet)
+                    wb.save(divide_after + "\\" + save_file_name + name + ").xlsx")
 
 # %%
-'''
-wb = opxl.load_workbook("02_分割するだけ\\01_分割前\現場評価シート（DDC様向け）_20231110_評価済み.xlsx")
-ws =
-'''
