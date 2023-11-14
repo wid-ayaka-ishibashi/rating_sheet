e25343c3d42e5144808046830a7304c3ca120f27
wid-ayaka-ishibashi <aya_ishibashi@worksid.co.jp>
2023-11-15 01:30:52 +0900

    分割コード完成

--- 02_分割するだけ/divide.py
+++ 02_分割するだけ/divide.py
@@ -5,29 +5,24 @@ from IPython.display import display
 
 divide_pre = "01_分割前"
 divide_after = "02_分割後"
-save_file_name1 = "2023年上期評価シート("
+save_file_name = "2023年上期評価シート("
 
-for folder, subFolder, files in os.walk(divide_pre):
-    for file in files:
-#        ext = os.path.splitext(file)[0] #拡張子変更考慮
-        if "_評価済み" in file:
-            wb = px.load_workbook(folder + "\\" + file)
-            i = 0
-            for sheetname in wb.sheetnames:
-                if "【" and "】" not in sheetname:
-                    i = i+1
-                try:
-                    ws_target = wb.worksheets[i].title
-                except:
-                    break
-                    ws = wb[sheetname]
-                    peple_fullname = ws["A2"].value
-                    for sheetname in wb.sheetnames:
-                        #[ws_target]以外は削除
-                        if sheetname.title == ws_target:
-                            continue
-                        else:
-                            wb.remove(sheetname)
-                        new_wb.save(divide_after + "\\" + save_file_name1 + peple_fullname + ").xlsx")
+folder, subFolder, files in os.walk(divide_pre)
+for file in files:
+#       ext = os.path.splitext(file)[0] #拡張子変更考慮
+#        files = [i for i in files if "_評価済み" in file]
+    wb = px.load_workbook(folder + "\\" + file)
+    sheetnames = wb.sheetnames
+    sheetnames = [i for i in sheetnames if ("【" and "】") not in i]
+    for sheetname in sheetnames:
+        wb = px.load_workbook(folder + "\\" + file)
+        for ws in wb.worksheets:
+            if ws.title == sheetname:
+                continue
+            else:
+                wb.remove(ws)
+            ws = wb[sheetname]
+            name = ws["A2"].value
+        wb.save(divide_after + "\\" + save_file_name + name + ").xlsx")
 
 # %%
